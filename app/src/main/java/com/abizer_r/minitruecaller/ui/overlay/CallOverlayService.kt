3b74bfd42498a4b3e110a017c39a7cbb087b834c
rabiz <rabizeridris@gmail.com>
2025-07-25 12:34:59 +0530

    switch to xml for caller info overlay

--- app/src/main/java/com/abizer_r/minitruecaller/ui/overlay/CallOverlayService.kt
+++ app/src/main/java/com/abizer_r/minitruecaller/ui/overlay/CallOverlayService.kt
@@ -10,10 +10,15 @@ import android.os.Looper
 import android.telecom.Call
 import android.util.Log
 import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.compose.ui.platform.ComposeView
+import androidx.core.view.isVisible
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.abizer_r.minitruecaller.R
 import com.abizer_r.minitruecaller.domain.model.CallerInfo
 import com.abizer_r.minitruecaller.utils.Constants
 import kotlinx.coroutines.CoroutineScope
@@ -25,41 +30,57 @@ import kotlinx.coroutines.launch
 class CallOverlayService: Service() {
 
     private lateinit var windowManager: WindowManager
-    private lateinit var composeView: ComposeView
+    private var overlayView: View? = null
 
     private val callerInfoFlow = MutableStateFlow<CallerInfo?>(null)
 
-    override fun onCreate() {
-        super.onCreate()
-        composeView = ComposeView(this).apply {
-            setContent {
-                val callerInfo = callerInfoFlow.collectAsStateWithLifecycle()
-                CallerInfoOverlay(callerInfo.value)
-            }
-        }
-    }
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val number = intent?.getStringExtra(Constants.INCOMING_CALL_EXTRA)
+            ?: return START_NOT_STICKY
         Log.i("CallOverlayService", "onStartCommand: number = $number")
-        if (number != null) {
-            fetchCallerInfo(number)
-            showOverlay()
-        }
+        showOverlay()
+        fetchCallerInfo(number)
         return START_STICKY
     }
 
     private fun fetchCallerInfo(number: String) {
         CoroutineScope(Dispatchers.IO).launch {
-            delay(1000)
+            delay(2000)
             callerInfoFlow.value = CallerInfo(
                 number = number,
                 name = "Dummy Name"
             )
         }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            callerInfoFlow.collect { callerInfo ->
+
+                overlayView?.apply {
+                    val nameTextView = findViewById<TextView>(R.id.tvName)
+                    nameTextView.text = callerInfo?.name
+
+                    val numberTextView = findViewById<TextView>(R.id.tvNumber)
+                    numberTextView.text = callerInfo?.number
+
+                    val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+                    progressBar.isVisible = false
+                    nameTextView.isVisible = true
+                    numberTextView.isVisible = true
+                }
+            }
+        }
     }
 
     private fun showOverlay() {
+
+        val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        overlayView = inflater.inflate(R.layout.overlay_caller_info, null)
+
+
+
+
+
         val params = WindowManager.LayoutParams(
             WindowManager.LayoutParams.WRAP_CONTENT,
             WindowManager.LayoutParams.WRAP_CONTENT,
@@ -75,7 +96,7 @@ class CallOverlayService: Service() {
         params.y = 100
 
         windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
-        windowManager.addView(composeView, params)
+        windowManager.addView(overlayView, params)
 
         Handler(Looper.getMainLooper()).postDelayed({
             stopSelf()
@@ -84,10 +105,11 @@ class CallOverlayService: Service() {
     }
 
     override fun onDestroy() {
-        super.onDestroy()
-        if (::composeView.isInitialized) {
-            windowManager.removeView(composeView)
+        overlayView?.let {
+            windowManager.removeView(it)
+            overlayView = null
         }
+        super.onDestroy()
     }
 
 
