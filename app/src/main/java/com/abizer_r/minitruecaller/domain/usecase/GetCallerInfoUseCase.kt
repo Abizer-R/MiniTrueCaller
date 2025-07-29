c27ff04818cf59c590b345630897a29539438e2f
rabiz <rabizeridris@gmail.com>
2025-07-29 16:10:55 +0530

    Use flow for function return type in usecases

--- app/src/main/java/com/abizer_r/minitruecaller/domain/usecase/GetCallerInfoUseCase.kt
+++ app/src/main/java/com/abizer_r/minitruecaller/domain/usecase/GetCallerInfoUseCase.kt
@@ -3,11 +3,19 @@ package com.abizer_r.minitruecaller.domain.usecase
 import com.abizer_r.minitruecaller.data.repository.ResultData
 import com.abizer_r.minitruecaller.domain.model.CallerInfo
 import com.abizer_r.minitruecaller.domain.repository.CallerRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class GetCallerInfoUseCase(
     private val repository: CallerRepository
 ) {
-    suspend operator fun invoke(number: String): ResultData<CallerInfo> {
-        return repository.getCallerInfo(number)
-    }
+    suspend operator fun invoke(number: String) = flow {
+        emit(ResultData.Loading())
+        val result = repository.getCallerInfo(number)
+        emit(result)
+    }.catch { e ->
+        emit(ResultData.Failed(e))
+    }.flowOn(Dispatchers.IO)
 }
